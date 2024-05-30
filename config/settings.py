a6ea04b2b84320f7447ec5ee86f4b3afb9746216
Андрей Метляев <andrejmetlaev@MacBook-Pro-Andrej.local>
2024-05-30 15:27:09 +0700

    Add new fields

--- config/settings.py
+++ config/settings.py
@@ -78,13 +78,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': env.str('DB_NAME'),
-        'USER': env.str('DB_USER'),
-        'PASSWORD': env.str('DB_PASSWORD'),
-        'HOST': env.str('DB_HOST'),
-        'PORT': env.int('DB_PORT')
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
     }
 }
 
