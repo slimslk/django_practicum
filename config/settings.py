cc9c5c96caf0b6c191685e344c45b52cd01a5d87
Dimm <slim.slk@gmail.com>
2024-05-30 11:13:06 +0200

    added verbose name to the author's fields

--- config/settings.py
+++ config/settings.py
@@ -76,12 +76,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'
+if env.bool('MYSQL', default=False):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': env.str('DB_NAME'),
+            'USER': env.str('DB_USER'),
+            'PASSWORD': env.str('DB_PASSWORD'),
+            'HOST': env.str('DB_HOST'),
+            'PORT': env.str('DB_PORT')
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
--- library/models/author.py
+++ library/models/author.py
@@ -4,9 +4,17 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Author(models.Model):
 
-    name = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
-    birth_date = models.DateField()
-    profile = models.URLField(max_length=255, null=True, blank=True)
-    deleted = models.BooleanField(default=False)
-    rating = models.FloatField(default=1, validators=[MinValueValidator(1), MaxValueValidator(10)])
+    name = models.CharField(max_length=100, verbose_name='Имя автора')
+    surname = models.CharField(max_length=100, verbose_name='Фамилия автора')
+    birth_date = models.DateField(verbose_name='Дата рождения')
+    profile = models.URLField(max_length=255, null=True, blank=True, verbose_name='Ссылка на профиль')
+    deleted = models.BooleanField(
+        default=False,
+        verbose_name='Удалено',
+        help_text='Если True аккаунт удален, если False аккаунт активен'
+    )
+    rating = models.FloatField(
+        default=1,
+        validators=[MinValueValidator(1),MaxValueValidator(10)],
+        verbose_name='Рейтинг автора'
+    )
